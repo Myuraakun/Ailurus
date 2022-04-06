2ae5cf95e2099318a5289fdeac2a2f2f567115f5
Grabenkind <stevenmueller99@outlook.de>
2022-04-06 15:02:58 +0200

    - added new item: Hemp Fabric - added new item: Hemp Buds (dried) - added new recipe: Hemp Fabric - added new recipe: Hemp Buds (dried) - changed some textures

--- src/main/java/net/myuraakun/ailurus/item/ModItems.java
+++ src/main/java/net/myuraakun/ailurus/item/ModItems.java
@@ -11,6 +11,8 @@ public class ModItems {
 	public static final Item HEMP = registerItem("hemp", new Item(new FabricItemSettings().group(ModItemGroup.NATURAL)));
 	public static final Item HEMP_SEEDS = registerItem("hemp_seeds", new Item(new FabricItemSettings().group(ModItemGroup.NATURAL)));
 	public static final Item HEMP_BUDS = registerItem("hemp_buds", new Item(new FabricItemSettings().group(ModItemGroup.NATURAL)));
+	public static final Item HEMP_BUDS_DRY = registerItem("hemp_buds_dry", new Item(new FabricItemSettings().group(ModItemGroup.NATURAL)));
+	public static final Item HEMP_FABRIC = registerItem("hemp_fabric", new Item(new FabricItemSettings().group(ModItemGroup.NATURAL)));
 
 	private static Item registerItem(String name, Item item) {
 		return Registry.register(Registry.ITEM, new Identifier(Ailurus.MOD_ID, name), item);
